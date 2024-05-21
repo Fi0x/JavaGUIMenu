6c6918045f1453a70b292c5a44138c521eae71ba
Fi0x <felpech@aol.com>
2024-05-21 18:02:19 +0200

    Fixed test

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -2,26 +2,22 @@ import io.fi0x.javaguimenu.GUIWindow;
 import io.fi0x.javaguimenu.elements.PriorityButton;
 import io.fi0x.javaguimenu.elements.RegularButton;
 import io.fi0x.javaguimenu.layouts.LayoutTypes;
-import io.fi0x.javalogger.logging.Logger;
 
 public class TestMain
 {
     public static void main(String[] args)
     {
-        Logger.getInstance().setVerbose(true);
-        Logger.getInstance().setDebug(true);
-
         RegularButton btn1 = new RegularButton();
         btn1.setColIdx(0);
         btn1.setRowIdx(0);
-        btn1.setText("Hallo");
+        btn1.setButtonText("Hallo");
         btn1.setColSpan(2);
         GUIWindow.addElement(btn1);
 
         PriorityButton btn2 = new PriorityButton();
         btn2.setColIdx(0);
         btn2.setRowIdx(1);
-        btn2.setText("Priority");
+        btn2.setButtonText("Priority");
         GUIWindow.addElement(btn2);
 
         GUIWindow.setColumns(3);
